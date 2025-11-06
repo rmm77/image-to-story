5e62b9bea5e326523ab13526b851c58c94ba454b
rmm77 <rmm77@students.uwf.edu>
2025-11-06 15:46:28 -0500

    Update app.py

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ def serve_home():
 # --- S3 client for pre-signed URLs ---
 s3 = boto3.client(
     "s3",
-    region_name=os.getenv("AWS_REGION", "us-east-1"),
+    region_name=os.getenv("AWS_REGION", "us-east-2"),
     aws_access_key_id=os.getenv("AWS_ACCESS_KEY_ID"),
     aws_secret_access_key=os.getenv("AWS_SECRET_ACCESS_KEY"),
 )
